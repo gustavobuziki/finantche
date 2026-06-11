e05005dd94674b25d4796e7fc8295bd42ebc554c
Gustavo Buziki <gbuziki@gmail.com>
2026-06-11 08:20:01 -0300

    refactor: show chart annual

--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -10,6 +10,7 @@ import { usePeriod } from "@/hooks/use-period";
 import { QUERY_KEYS } from "@/constants/query-keys";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { DrawerCreateExpense } from "@/components/expenses/drawer-create-expense";
+import { ChartAnnual } from "@/components/expenses/chart-annual";
 
 export function Dashboard() {
   const { period } = usePeriod();
@@ -26,7 +27,7 @@ export function Dashboard() {
       <main className="flex flex-col gap-3 pb-6 -mt-3">
         <div className="flex flex-col md:flex-row gap-3">
           <Buckets expenses={expenses} />
-          {/* <ChartAnnual /> */}
+          {!isMobile && <ChartAnnual />}
         </div>
         {!isMobile ? (
           <TableExpenses expenses={expenses} />
